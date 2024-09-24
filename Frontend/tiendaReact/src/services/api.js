46fdfda449e694ff301c35a016d730e11114a19f
MartyPR <martin.p1131@gmail.com>
2024-09-24 13:58:02 -0500

    remove frontend folder

--- Frontend/tiendaReact/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://fakeapi.platzi.com',
-});
-
-export const getProductos = () => api.get('/products');
-export const getProductoDetalle = (id) => api.get(`/products/${id}`);
--- obj/Debug/net8.0/Challenge Double V Partners.AssemblyInfo.cs
+++ obj/Debug/net8.0/Challenge Double V Partners.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Challenge Double V Partners")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa75205314c61b51ea30d58c2ed72bb213c5172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03654b951ffd9d44408a1d9f6bf772cb0c5d82e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Challenge Double V Partners")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Challenge Double V Partners")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
